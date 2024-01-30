117a72816af682b19cdc6b41f9160c21089d2951
Antonio Marucci <antoniomarucci13@gmail.com>
2024-01-30 16:52:09 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleApp2/Persona.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Persona
+    {
+        public string Nome { get; set; }
+
+        public string Cognome { get; set; }
+
+        public int Eta { get; set; }
+        public Persona(string Nome, string Cognome, int Eta)
+        {
+            this.Nome = Nome;
+            this.Cognome = Cognome;
+            this.Eta = Eta;
+        }
+
+        public void GetNome()
+        {
+            Console.WriteLine("Inserire il nome: ");
+            Nome = Console.ReadLine();
+        }
+        public void GetCognome()
+        {
+            Console.WriteLine("Inserire il cognome: ");
+            Cognome = Console.ReadLine();
+        }
+        public void GetEta()
+        {
+            Console.WriteLine("Inserisci l'eta': ");
+            Eta = int.Parse(Console.ReadLine());
+        }
+        public void GetDettagli()
+        {
+            Console.WriteLine($"La persona si chiama: {Nome}, il suo cognome e' {Cognome} e ha {Eta} anni.");
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona("", "", 0);
+            persona.GetNome();
+            persona.GetCognome();
+            persona.GetEta();
+            persona.GetDettagli();
+
+            Console.WriteLine("Premere Invio per uscire");
+            Console.ReadLine();
+        }
+    }
+}
